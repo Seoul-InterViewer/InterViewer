c8f523bdd822dede61c395d36f1a3a49f577d9ab
GiToon10100011 <Boon10034@gmail.com>
2025-05-15 14:26:21 +0900

    chore: boilerplate 완성

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function NotFound() {
+export default function Error404Page() {
   return <div></div>;
 }
---
+++ src/app/components/list/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import List from "./List";
+
+export default function ListTestPage() {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+}
