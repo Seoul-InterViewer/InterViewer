02ade8e2e3ded85cf0a7b0691031f0fa12466ab0
Hoon <ehdgns730@gmail.com>
2025-05-18 20:23:26 +0900

    test: 모든 아이콘 출력 테스트
    -프롭스값 전달 확인 및 테스트

--- src/app/components/icon/icon.variants.ts
+++ src/app/components/icon/icon.variants.ts
@@ -1,8 +1,8 @@
-import { CloseSize } from "./icon.type";
+import { tv } from "tailwind-variants";
 
-export const closeSizeStyles: Record<CloseSize, string> = {
-  modal: "text-black w-7 h-7",
-  login: "text-var(--color-font-gray) w-9 h-9 ",
-  tag: "text-var(--color-font-gray) w-[13px] h-[13px",
-  blank: "text-black w-6 h-6",
-};
+export const iconVariants = tv({
+  base: "",
+  variants: {
+    color: {},
+  },
+});
