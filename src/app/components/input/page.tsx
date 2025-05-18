2811df76471e25619ce0221432adbfae507aac0e
GiToon10100011 <boon10034@gmail.com>
2025-05-18 21:13:10 +0900

    style: 반응형 스타일 변수 추가 확장

--- src/app/components/input/page.tsx
+++ src/app/components/input/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Input } from "./Input";
 import { inputVariants } from "./input.variants";
+import { toTailwind } from "@/styles/utils";
 export default function InputTestPage() {
   return (
     <div className="p-20 flex flex-col gap-4">
@@ -12,6 +13,23 @@ export default function InputTestPage() {
         className={inputVariants({ variant: "withBg" })}
       />
       <Input type="text" name="name" id="name" placeholder="name" />
+      <div className={`flex h-20`}>
+        <Input
+          className={inputVariants({ withButton: true })}
+          type="text"
+          name="name"
+          id="name"
+          placeholder="name"
+        />
+        <button className="bg-black text-white p-2 h-full flex-1">test</button>
+      </div>
+      <Input
+        className={inputVariants({ search: true })}
+        type="text"
+        name="name"
+        id="name"
+        placeholder="name"
+      />
     </div>
   );
 }
