4bd958c0ecee50c9a741baa6e022a426d7e040fd
Hoon <ehdgns730@gmail.com>
2025-05-19 21:02:31 +0900

    style: variant 수정
    -page 파일에 적용

--- src/app/components/modal/page.tsx
+++ src/app/components/modal/page.tsx
@@ -12,6 +12,22 @@ function ModalPage() {
         모달 열기
       </button>
       <div>
+        {/* <Modal
+          size="default"
+          buttonLocation="left"
+          isOpen={isOpen}
+          onClose={close}
+          title="정말로 종료하시겠습니까?"
+          showCloseButton={true}
+          buttons={[
+            <button className="bg-black text-white px-4 py-2 rounded-md" onClick={close}>
+              취소
+            </button>,
+            <button className="bg-main text-black px-4 py-2 rounded-md" onClick={close}>
+              확인
+            </button>,
+          ]}
+        /> */}
         <Modal
           size="default"
           buttonLocation="left"
