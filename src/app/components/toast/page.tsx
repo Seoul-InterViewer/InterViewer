c364998579825815266faace281ebffdaa7e9e79
GiToon10100011 <Boon10034@gmail.com>
2025-05-20 12:44:12 +0900

    feat: 삭제 기능 onRemove 추가

--- src/app/components/toast/page.tsx
+++ src/app/components/toast/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import React from "react";
 import { Toast } from "./Toast";
 
 export default function ToastTestPage() {
+  const handleRemove = (e: React.MouseEvent<HTMLButtonElement>) =>
+    console.log(e.currentTarget.closest("div"));
+
   return (
     <div>
-      <Toast content="Toast 테스트 1" />
+      <Toast content="Toast 테스트 1" onRemove={handleRemove} />
       <Toast content="에러 Toast 테스트 1" type="error" />
     </div>
   );
