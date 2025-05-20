069ab9e562544e44f607a1463455eb48c0f5bf15
GiToon10100011 <Boon10034@gmail.com>
2025-05-20 13:06:40 +0900

    style: Toast gap 조정

--- src/app/components/toast/Toast.tsx
+++ src/app/components/toast/Toast.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { IToastProps } from "./toast.type";
 import { motion } from "motion/react";
@@ -10,13 +10,16 @@ export const Toast = ({ content, type = "success", onRemove }: IToastProps) => {
       initial={{ opacity: 0, x: 50 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: 50 }}
-      className="relative min-w-64 bg-white text-regular-18 text-black p-3 shadow-lg mb-2"
+      className="relative min-w-64 bg-white text-medium-12 text-black p-3 shadow-lg mb-2 md:text-regular-18"
     >
-      <span>{`${type === "error" ? "❌" : "✅"} ${content}`}</span>
+      <p className="flex gap-2">
+        <span>{type === "error" ? "❌" : "✅"}</span>
+        {content}
+      </p>
 
       {onRemove && (
         <button
-          className="w-3 h-3 absolute top-1/2 right-1 -translate-y-1/2 border-none"
+          className="w-3 h-3 absolute top-1/2 right-1 -translate-y-1/2 border-none fill-sub-text"
           onClick={onRemove}
         >
           <Icon name="close" />
