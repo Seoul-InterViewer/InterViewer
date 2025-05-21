ec8677f884698432a31f79bf83e88a056430e43c
GiToon10100011 <Boon10034@gmail.com>
2025-05-21 12:16:39 +0900

    feat: ListItem 추가

---
+++ src/app/components/list/listItem/index.ts
@@ -0,0 +1,3 @@
+export * from "./ListItem"
+export * from "./listItem.type"
+export * from "./listItem.variants"
---
+++ src/app/components/list/listItem/listItem.variants.ts
@@ -0,0 +1,12 @@
+import { tv } from "tailwind-variants";
+
+export const listItemVariants = tv({
+  base: "",
+  variants: {
+    type: {
+      searchResult: "",
+      image: "",
+      
+    },
+  },
+});
