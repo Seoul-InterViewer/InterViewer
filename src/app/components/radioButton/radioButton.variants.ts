be93873520a9c553c0c38d5e2512b72918be2ca8
박리아 <danny6217@gmail.com>
2025-05-21 23:03:00 +0900

    style: 라디오버튼컴포넌트 variants에서 slot제거, dev merge 후 폰트 사이즈 변경 #11

--- src/app/components/radioButton/radioButton.variants.ts
+++ src/app/components/radioButton/radioButton.variants.ts
@@ -1,14 +1,11 @@
 import { tv } from "tailwind-variants";
 
 export const radioButtonVariants = tv({
-  slots: {
-    wrapper: "w-full flex items-center gap-4",
-    item: "px-3.5 py-1.5 rounded-lg border cursor-pointer",
-  },
+  base: "px-3.5 py-1.5 rounded-lg border font-sb-14 cursor-pointer",
   variants: {
     variant: {
-      default: { item: "border-black/20 bg-white text-font-gray" },
-      selected: { item: "border-transparent bg-main text-white" },
+      default: "border-black/20 bg-white text-font-gray",
+      selected: "border-transparent bg-main text-white",
     },
   },
   defaultVariants: {
