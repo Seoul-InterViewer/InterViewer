c039a3ec139b602e9d5dedaaffbf5892d9c85672
GiToon10100011 <Boon10034@gmail.com>
2025-05-21 20:15:35 +0900

    refactor: 불필요한 import 제거

--- src/app/components/list/List.tsx
+++ src/app/components/list/List.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { listVariants } from "./list.variants";
 import { IListProps } from "./list.type";
 
 const List = ({ className, children }: IListProps) => {
