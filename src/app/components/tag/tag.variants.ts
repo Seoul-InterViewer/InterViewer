7139ccf9255e8025ea030b4498de57a0a3b47f11
u-ulim <uiissoop@gmail.com>
2025-05-22 14:10:18 +0900

    test: 테스트

--- src/app/components/tag/tag.variants.ts
+++ src/app/components/tag/tag.variants.ts
@@ -5,7 +5,7 @@ export const tagVariants = tv({
   variants: {
     type: {
       default:
-        "bg-[var(--color-tag)] text-gray-800 border-[var(--color-tag)] px-[7px] py-[3px] text-sb-12",
+        "bg-[var(--color-tag)] text-font border-[var(--color-tag)] px-[7px] py-[3px] font-sb-12",
       card: "bg-white text-gray-800 border-gray-300 text-medium-8 px-[5px] py-[3px] md:text-regular-14 md:px-[10px] md:py-[5px]",
       chooseTag:
         "bg-white text-[var(--color-font-gray)] border-gray-300 px-[7px] py-[3px] text-sb-12 block text-medium-10 md:text-regular-14",
