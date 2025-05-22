54be102bd7e0c0303490e186631958c2f1462b67
GiToon10100011 <Boon10034@gmail.com>
2025-05-22 19:11:12 +0900

    feat: Slider 컴포넌트 기초 기반 설계

---
+++ src/app/components/slider/Slider.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+export const Slider = () => {
+  return <Swiper></Swiper>;
+};
---
+++ src/app/components/slider/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SliderTestPage() {
+  return <div></div>;
+}
