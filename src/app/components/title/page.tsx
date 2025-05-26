3efbe5384c4599c73219ccb595fc478f601c6f86
Hoon <ehdgns730@gmail.com>
2025-05-26 20:55:30 +0900

    test: page 출력 테스트
    -variants 종류 별 출력 테스트

--- src/app/components/title/page.tsx
+++ src/app/components/title/page.tsx
@@ -1,18 +1,13 @@
-import { Icon } from "../icon";
 import Title from "./Title";
 import { titleVariants } from "./title.variants";
 export default function page() {
-  //  타이틀 컴포넌트  
+  //  타이틀 컴포넌트
   // 사이즈 lg, md, sm, className(직접입력)으로 스타일링 하시면 됩니다
-  
+
   // 모든 텍스트는 children으로 받기 때문에 쌍태그를 이용하여 텍스트를 넣어주시면 됩니다.
-
-  // 텍스트앞에 아이콘 및 이모지는 span 태그와 Icon컴포넌트 등을 이용하여 넣어주시면됩니다.
-
 
   return (
     <div className="md:w-[1260px] h-[100vh] mx-auto w-full flex flex-col gap-10 mt-10">
-
       <Title className={titleVariants({ size: "lg" })}>
         Q. React에서 useMemo와 useCallback의 차이점은 무엇인가요? 언제 사용하나요?
       </Title>
@@ -21,11 +16,9 @@ export default function page() {
         <span>📝&nbsp;</span>해설
       </Title>
 
-      <Title className="font-sb-28">
-        <Icon name="codepen" className="md:w-6 md:h-6" />
-        CODEPEN URL
-      </Title>
+      <Title className={titleVariants({ size: "md" })}>(1) 전역 공간에서의 this</Title>
 
+      {/* 텍스트 사이즈가 서로 다를 경우 */}
       <Title className={titleVariants({ size: "sm" })}>
         <span className="md:font-bold-32 font-bold-24">Q.&nbsp;</span>javascript
       </Title>
--- src/app/components/title/title.variants.ts
+++ src/app/components/title/title.variants.ts
@@ -4,7 +4,7 @@ export const titleVariants = tv({
   variants: {
     size: {
       lg: "md:font-bold-56 font-bold-32",
-      md: "md:font-bold-40 font-bold-32",
+      md: "md:font-bold-40 font-sb-28",
       sm: "md:font-sb-28 font-sb-20",
     },
   },
