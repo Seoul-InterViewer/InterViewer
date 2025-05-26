48b9d8da30e8b7f75fd57f0f9da7e2c5763258a4
박리아 <danny6217@gmail.com>
2025-05-26 15:07:35 +0900

    style: pagination 버튼 정렬 변경

--- src/app/components/pagination/Pagination.tsx
+++ src/app/components/pagination/Pagination.tsx
@@ -27,20 +27,22 @@ export const Pagination = ({
   };
 
   return (
-    <div className="flex gap-1 justify-center items-center">
-      <button
-        className={paginationVariants({ firstLastButton: true, disabled: currentPage === 1 })}
-        onClick={() => movePage(1)}
-      >
-        <Icon name="chevronLeft" className="h-4 absolute top-1/2 right-1/3 -translate-y-1/2" />
-        <Icon name="chevronLeft" className="h-4 absolute top-1/2 left-1/3 -translate-y-1/2" />
-      </button>
-      <button
-        className={paginationVariants({ disabled: noPrev })}
-        onClick={() => movePage(start - pageCount)}
-      >
-        <Icon name="chevronLeft" className="h-4" />
-      </button>
+    <div className="w-fit flex gap-1 justify-center items-center relative mx-auto">
+      <div className="absolute right-full -translate-x-1 flex gap-1">
+        <button
+          className={paginationVariants({ firstLastButton: true, disabled: currentPage === 1 })}
+          onClick={() => movePage(1)}
+        >
+          <Icon name="chevronLeft" className="h-4 absolute top-1/2 right-1/3 -translate-y-1/2" />
+          <Icon name="chevronLeft" className="h-4 absolute top-1/2 left-1/3 -translate-y-1/2" />
+        </button>
+        <button
+          className={paginationVariants({ disabled: noPrev })}
+          onClick={() => movePage(start - pageCount)}
+        >
+          <Icon name="chevronLeft" className="h-4" />
+        </button>
+      </div>
 
       {visiblePages.map((page) => (
         <button
@@ -54,22 +56,24 @@ export const Pagination = ({
         </button>
       ))}
 
-      <button
-        className={paginationVariants({ disabled: noNext })}
-        onClick={() => movePage(start + pageCount)}
-      >
-        <Icon name="chevronRight" className="h-4" />
-      </button>
-      <button
-        className={paginationVariants({
-          firstLastButton: true,
-          disabled: currentPage === totalPages,
-        })}
-        onClick={() => movePage(totalPages)}
-      >
-        <Icon name="chevronRight" className="h-4 absolute top-1/2 right-1/3 -translate-y-1/2" />
-        <Icon name="chevronRight" className="h-4 absolute top-1/2 left-1/3 -translate-y-1/2" />
-      </button>
+      <div className="absolute left-full translate-x-1 flex gap-1">
+        <button
+          className={paginationVariants({ disabled: noNext })}
+          onClick={() => movePage(start + pageCount)}
+        >
+          <Icon name="chevronRight" className="h-4" />
+        </button>
+        <button
+          className={paginationVariants({
+            firstLastButton: true,
+            disabled: currentPage === totalPages,
+          })}
+          onClick={() => movePage(totalPages)}
+        >
+          <Icon name="chevronRight" className="h-4 absolute top-1/2 right-1/3 -translate-y-1/2" />
+          <Icon name="chevronRight" className="h-4 absolute top-1/2 left-1/3 -translate-y-1/2" />
+        </button>
+      </div>
     </div>
   );
 };
