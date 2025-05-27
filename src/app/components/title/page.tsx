7858c2fc8be5b91bf9f438436a8e4c03d2db9701
Hoon <ehdgns730@gmail.com>
2025-05-27 14:24:05 +0900

    refactor: page.tsx 수정
    - arbitrary 사용 자제 -> w-pc, h-screen으로 변경

--- src/app/components/title/page.tsx
+++ src/app/components/title/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
   // 모든 텍스트는 children으로 받기 때문에 쌍태그를 이용하여 텍스트를 넣어주시면 됩니다.
 
   return (
-    <div className="md:w-[1260px] h-[100vh] mx-auto w-full flex flex-col gap-10 mt-10">
+    <div className="md:w-pc h-screen mx-auto w-full flex flex-col gap-10 mt-10">
       <Title
         size="lg"
         title="   Q. React에서 useMemo와 useCallback의 차이점은 무엇인가요? 언제 사용하나요?"
