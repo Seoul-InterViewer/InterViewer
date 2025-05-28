a7dc87ead1f28d5d0d80c7fc8e440887130e6029
u-ulim <uiissoop@gmail.com>
2025-05-28 13:14:31 +0900

    refactor: 리팩토링

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { cardVariants } from "./card.variants";
 import { ICardProps } from "./card.type";
 
-export const Card = ({ type, children }: ICardProps) => {
+export const Card = ({ type = "mainCard", children }: ICardProps) => {
   return <div className={cardVariants({ type })}>{children}</div>;
 };
