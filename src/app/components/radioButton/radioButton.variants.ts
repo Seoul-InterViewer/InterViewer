78a5b4ebf5c8d9f7ca4c4d44af787f268a66098d
u-ulim <uiissoop@gmail.com>
2025-05-29 16:52:07 +0900

    style: font 추가 - font-sb-12 / md:font-sb-14 추가

--- src/app/components/radioButton/radioButton.variants.ts
+++ src/app/components/radioButton/radioButton.variants.ts
@@ -1,7 +1,7 @@
 import { tv } from "tailwind-variants";
 
 export const radioButtonVariants = tv({
-  base: "px-3.5 py-1.5 rounded-lg border font-sb-14 cursor-pointer transition-all duration-200 hover:opacity-80",
+  base: "px-3.5 py-1.5 rounded-lg border font-sb-14 cursor-pointer transition-all duration-200 hover:opacity-80 font-sb-12 md:font-sb-14",
   variants: {
     variant: {
       default: "border-black/20 bg-white text-font-gray",
