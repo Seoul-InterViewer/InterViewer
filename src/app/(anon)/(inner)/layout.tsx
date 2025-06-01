c0b7e7b3754005fbbcf68e9f8235bc95351cf447
GiToon10100011 <Boon10034@gmail.com>
2025-06-01 15:26:08 +0900

    style: 반응형 레이아웃 수정

--- src/app/(anon)/(inner)/layout.tsx
+++ src/app/(anon)/(inner)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="w-full mt-25 md:w-pc mx-auto">{children}</div>;
+  return <div className="w-full mt-10 md:mt-25 md:w-pc mx-auto">{children}</div>;
 }
