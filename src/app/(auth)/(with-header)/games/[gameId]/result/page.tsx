4f693835652826a57dac8806e4596ba7bcf5111e
u-ulim <uiissoop@gmail.com>
2025-06-04 11:23:38 +0900

    test: 테스트

--- src/app/(auth)/(with-header)/games/[gameId]/result/page.tsx
+++ src/app/(auth)/(with-header)/games/[gameId]/result/page.tsx
@@ -1,5 +1,32 @@
+import { BreadCrumb } from "@/app/components/breadCrumb";
 import React from "react";
 
-export const GameResultPage = () => {
-  return <div>page</div>;
-};
+const items = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "내 게임",
+    href: "/",
+  },
+  {
+    label: "테스트 결과",
+    href: "/",
+  },
+];
+
+export default function GameResultPage() {
+  return (
+    <div className="w-full">
+      {/* Questions */}
+      <div>
+        <BreadCrumb items={items} />
+        <div>sadfsdf</div>
+      </div>
+
+      {/* Result Sidebar */}
+      <div></div>
+    </div>
+  );
+}
