ba7b6ad9d21858c33ef487acae048bcfec02200d
GiToon10100011 <Boon10034@gmail.com>
2025-06-05 08:30:20 +0900

    feat: MainCard 구조설정

--- src/app/(anon)/components/mainCard/MainCard.tsx
+++ src/app/(anon)/components/mainCard/MainCard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Card } from "@/app/components/card";
 import { IMainCardProps } from "./mainCard.type";
+import { Icon } from "@/app/components/icon";
+import { Tag } from "@/app/components/tag";
+import { List, listVariants } from "@/app/components/list";
 
 export const MainCard = ({
   title,
@@ -9,16 +12,37 @@ export const MainCard = ({
   tags,
   likes,
   publishedAt,
+  category,
+  difficulty,
 }: IMainCardProps) => {
   return (
     <Card type="mainCard">
       <div>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <p>{imageUrl}</p>
-        <p>{tags}</p>
-        <p>{likes}</p>
-        <p>{publishedAt}</p>
+        <div>
+          <div className="flex justify-between">
+            <strong>
+              {category} | {`난이도: ${difficulty}`}
+            </strong>
+            <div>
+              <Icon name="bookmark" />
+            </div>
+          </div>
+          <List className={listVariants({ type: "tags" })}>
+            {tags.map((tag) => (
+              <Tag type="card" key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </List>
+        </div>
+        <div>
+          <strong></strong>
+          <p></p>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </Card>
   );
