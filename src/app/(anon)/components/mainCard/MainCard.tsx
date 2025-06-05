cf00df6bed2de12aeec32ae4e53a05c0f8803d72
GiToon10100011 <Boon10034@gmail.com>
2025-06-05 09:04:36 +0900

    style: MainCard 스타일링 조정

--- src/app/(anon)/components/mainCard/MainCard.tsx
+++ src/app/(anon)/components/mainCard/MainCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Card } from "@/app/components/card";
 import { IMainCardProps } from "./mainCard.type";
 import { Icon } from "@/app/components/icon";
@@ -15,35 +16,47 @@ export const MainCard = ({
   difficulty,
   author,
 }: IMainCardProps) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
   return (
     <Card type="mainCard">
-      <div>
-        <div>
-          <div className="flex justify-between items-center">
-            <strong>
-              {category} | {`난이도: ${difficulty}`}
-            </strong>
-            <div>
-              <Icon name="bookmark" strokeWidth={0.6} className="*:stroke-1" />
+      <div className="flex flex-col justify-between h-full">
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-3">
+            <div className="flex justify-between items-center">
+              <strong className="font-sb-24 text-sub-text">
+                {category} | {`난이도: ${difficulty}`}
+              </strong>
+              <div>
+                <Icon
+                  name="bookmark"
+                  onClick={() => setIsBookmarked((prev) => !prev)}
+                />
+              </div>
             </div>
+            <List type="tags">
+              {tags.map((tag) => (
+                <Tag type="card" key={tag} className="px-1">
+                  {tag}
+                </Tag>
+              ))}
+            </List>
           </div>
-          <List type="tags">
-            {tags.map((tag) => (
-              <Tag type="card" key={tag}>
-                {tag}
-              </Tag>
-            ))}
-          </List>
+          <div className="flex flex-col gap-4">
+            <strong className="font-sb-28 break-keep">{title}</strong>
+            <p className="font-regular-18 text-font-gray">{description}</p>
+          </div>
         </div>
-        <div>
-          <strong>{title}</strong>
-          <p>{description}</p>
-        </div>
-        <div>
-          <div>{author}</div>
-          <div>
-            <Icon name="heart" />
-            <span>{likes}</span>
+        <div className="flex justify-between items-center">
+          <p className="font-medium-12 text-font-gray">{`작성자: ${author}`}</p>
+          <div className="flex items-center gap-1">
+            <Icon
+              name="heart"
+              fill={isLiked ? "#FF4500" : "black"}
+              onClick={() => setIsLiked((prev) => !prev)}
+              className="w-4"
+            />
+            <span className="font-medium-12">{likes}</span>
           </div>
         </div>
       </div>
