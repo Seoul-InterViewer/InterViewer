c5c423a51aa1e49da4955c053311f1a649960d54
u-ulim <uiissoop@gmail.com>
2025-06-09 10:57:04 +0900

    style: card-mainCard 너비와 높이값 조정#63

--- src/app/components/card/card.variants.ts
+++ src/app/components/card/card.variants.ts
@@ -6,7 +6,7 @@ export const cardVariants = tv({
   // 2) size별로 slots 덮어쓰기
   variants: {
     type: {
-      mainCard: "w-44 h-44 py-2 px-2 md:px-4 md:py-4  md:w-104 md:h-104",
+      mainCard: "w-full h-fit min-h-55 py-2 px-2 md:px-4 md:py-4  md:w-104 md:h-104",
       favoriteCard: "w-40 h-40 py-2 px-2  md:w-100 md:h-100 md:py-8 md:px-8",
       editCard: "w-full h-58 py-2 px-2 md:w-full md:h-56 md:px-12 md:py-7",
       detailCard: "w-full h-61 py-6 px-7 md:w-full md:h-56 md:px-12 md:py-7",
