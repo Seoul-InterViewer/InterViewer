25c7965932d2adbd3819e0c34d62b29b997591de
GiToon10100011 <boon10034@gmail.com>
2025-06-10 20:14:00 +0900

    refactor: Input map 리펙토링

--- src/app/@modal/auth/join/components/JoinForm.tsx
+++ src/app/@modal/auth/join/components/JoinForm.tsx
@@ -12,71 +12,73 @@ const JoinForm = () => {
 
   const emailRef = useRef<HTMLInputElement>(null);
 
+  const inputFields = [
+    {
+      label: "이메일을 입력해주세요.",
+      type: "email",
+      placeholder: "이메일",
+      name: "email",
+      id: "email",
+      useInput: emailInput,
+      ref: emailRef,
+      withButton: false,
+    },
+    {
+      label: "닉네임을 입력해주세요.",
+      type: "text",
+      placeholder: "닉네임",
+      name: "nickname",
+      id: "nickname",
+      useInput: nicknameInput,
+      withButton: false,
+    },
+    {
+      label: "비밀번호를 입력해주세요.",
+      type: "password",
+      placeholder: "비밀번호",
+      name: "password",
+      id: "password",
+      useInput: passwordInput,
+      withButton: false,
+    },
+    {
+      label: "비밀번호를 입력해주세요.",
+      type: "password",
+      placeholder: "비밀번호 확인",
+      name: "passwordConfirm",
+      id: "passwordConfirm",
+      useInput: passwordConfirmInput,
+      withButton: true,
+    },
+  ];
+
   return (
     <form className="flex flex-col gap-6 md:gap-8">
-      <div className="md:h-15">
-        <Input
-          value={emailInput.value}
-          onChange={emailInput.handleChange}
-          type="email"
-          placeholder="이메일"
-          name="email"
-          id="email"
-          required
-          isCredential
-          className={inputVariants({ withButton: false, variant: "credentials" })}
-          label="이메일을 입력해주세요."
-          ref={emailRef}
-        />
-      </div>
-      <div className="md:h-15">
-        <Input
-          value={nicknameInput.value}
-          onChange={nicknameInput.handleChange}
-          type="text"
-          placeholder="닉네임"
-          name="nickname"
-          id="nickname"
-          required
-          isCredential
-          className={inputVariants({ withButton: false, variant: "credentials" })}
-          label="닉네임을 입력해주세요."
-        />
-      </div>
-      <div className="md:h-15"    >
-        <Input
-          value={passwordInput.value}
-          onChange={passwordInput.handleChange}
-          type="password"
-          placeholder="비밀번호"
-          name="password"
-          id="password"
-          required
-          isCredential
-          className={inputVariants({ withButton: false, variant: "credentials" })}
-          label="비밀번호를 입력해주세요."
-        />
-      </div>  
-      <div className="flex h-15">
-        <Input
-          value={passwordConfirmInput.value}
-          onChange={passwordConfirmInput.handleChange}
-          type="password"
-          placeholder="비밀번호 확인"
-          name="passwordConfirm"
-          id="passwordConfirm"
-          required
-          isCredential
-          className={inputVariants({ withButton: true, variant: "credentials" })}
-          label="비밀번호를 입력해주세요."
-        />
-        <Button
-          type="submit"
-          className={buttonVariants({ color: "black", class: "p-2 h-full flex-1 rounded-none" })}
-        >
-          회원가입
-        </Button>
-      </div>
+      {inputFields.map((field, index) => (
+        <div key={index} className={`md:h-15 ${index === 3 ? "flex h-15" : ""}`}>
+          <Input
+            value={field.useInput.value}
+            onChange={field.useInput.handleChange}
+            type={field.type}
+            placeholder={field.placeholder}
+            name={field.name}
+            id={field.id}
+            required
+            isCredential
+            className={inputVariants({ withButton: field.withButton, variant: "credentials" })}
+            label={field.label}
+            ref={field.ref}
+          />
+          {index === 3 && (
+            <Button
+              type="submit"
+              className={buttonVariants({ color: "black", class: "p-2 h-full flex-1 rounded-none" })}
+            >
+              회원가입
+            </Button>
+          )}
+        </div>
+      ))}
     </form>
   );
 };
