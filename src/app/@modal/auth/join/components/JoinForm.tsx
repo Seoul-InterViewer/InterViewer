4c86f5c2c57441a41169e4a2d08abf130b8e74d7
GiToon10100011 <Boon10034@gmail.com>
2025-06-10 19:41:46 +0900

    feat: JoinForm 구조화

--- src/app/@modal/auth/join/components/JoinForm.tsx
+++ src/app/@modal/auth/join/components/JoinForm.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Input } from "@/app/components/input";
 
 const JoinForm = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <form>
+      <Input type="text" placeholder="이메일" />
+      <Input type="text" placeholder="닉네임" />
+      <Input type="password" placeholder="비밀번호" />
+      <Input type="password" placeholder="비밀번호 확인" />
+    </form>
+  );
 };
 
 export default JoinForm;
