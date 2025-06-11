b05e713c8797aafccdb626207af80a81e6a7a506
u-ulim <uiissoop@gmail.com>
2025-06-11 22:08:23 +0900

    refactor: className props 추가 #94

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { cardVariants } from "./card.variants";
 import { ICardProps } from "./card.type";
 
-export const Card = ({ type = "mainCard", children }: ICardProps) => {
-  return <div className={cardVariants({ type })}>{children}</div>;
+export const Card = ({ type = "mainCard", children, className }: ICardProps) => {
+  return <div className={className || cardVariants({ type })}>{children}</div>;
 };
