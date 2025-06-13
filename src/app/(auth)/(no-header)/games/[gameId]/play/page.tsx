0bad305257b3293129af2b19465ce88379c2586e
u-ulim <uiissoop@gmail.com>
2025-06-13 09:06:46 +0900

    style: rouded 변경

--- src/app/(auth)/(no-header)/games/[gameId]/play/page.tsx
+++ src/app/(auth)/(no-header)/games/[gameId]/play/page.tsx
@@ -5,18 +5,24 @@ import React, { useState } from "react";
 import { RenderAnswerWithBlanks } from "./components/RenderAnswerWithBlanks/RenderAnswerWithBlanks";
 import { Buttons } from "./components/buttons/Buttons";
 import { Notice } from "./components/notice/Notice";
+import { Button } from "@/app/components/button";
+import { useRouter } from "next/navigation";
 
 export default function GamePlayPage() {
+  const router = useRouter();
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <main className="pt-20">
-      <div className="flex items-center gap-3 cursor-pointer mb-10">
+      <Button
+        className="flex items-center gap-3 cursor-pointer mb-10"
+        type="button"
+        onClick={() => router.back()}
+      >
         <Icon name="reply" />
         <span>뒤로</span>
-      </div>
-
-      <div className="w-full md:w-[70%]  min-h-[65vh] mx-auto flex flex-col justify-between">
+      </Button>
+      <div className="w-full md:w-[70%]  min-h-[67vh] mx-auto flex flex-col justify-between">
         <div>
           <Notice currentIndex={currentIndex} questions={questions} />
           <div className="font-sb-20 mb-4 !leading-8">
