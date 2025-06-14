3c68c92252e071c9ed732015afedcc2bdd5cd07b
박리아 <danny6217@gmail.com>
2025-06-15 01:28:13 +0900

    refactor: 시맨틱태그 main 교체

--- src/app/(auth)/(with-header)/settings/mock/settingPage.mock.ts
+++ src/app/(auth)/(with-header)/settings/mock/settingPage.mock.ts
@@ -1,1 +1,6 @@
-export const settingData = {};
+export const settingsData = {
+  nickname: "악성코드",
+  email: "interviewer@gmail.com",
+  gamePlayCount: 8,
+  gameRank: 24,
+};
--- src/app/(auth)/(with-header)/settings/page.tsx
+++ src/app/(auth)/(with-header)/settings/page.tsx
@@ -1,14 +1,18 @@
 import { BreadCrumb } from "@/app/components/breadCrumb";
+import { settingsData } from "./mock/settingPage.mock";
 
 export default function SettingsPage() {
   return (
-    <div>
+    <main className="w-full flex flex-col gap-7.5 md:pb-30 pb-15 border border-red-200">
       <BreadCrumb
         items={[
           { label: "Home", href: "/" },
           { label: "설정", href: "/settings" },
         ]}
       />
-    </div>
+      <div>
+        <h1>{settingsData.nickname}</h1>
+      </div>
+    </main>
   );
 }
