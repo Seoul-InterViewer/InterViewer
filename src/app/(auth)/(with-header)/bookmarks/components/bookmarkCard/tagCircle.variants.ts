8a8f67ab2e79c1252e24dfdc9a2a26038e9d8110
박리아 <danny6217@gmail.com>
2025-06-14 15:23:54 +0900

    style: mobile에서 hover 적용안된게 변경

--- src/app/(auth)/(with-header)/bookmarks/components/bookmarkCard/tagCircle.variants.ts
+++ src/app/(auth)/(with-header)/bookmarks/components/bookmarkCard/tagCircle.variants.ts
@@ -4,7 +4,7 @@ export const tagCircleVariants = tv({
   base: "w-7.5 md:w-15 h-7.5 md:h-15 bg-font rounded-full flex justify-center items-center overflow-hidden transition-all duration-200 group",
   variants: {
     variant: {
-      true: "hover:w-fit hover:px-6 hover:py-4",
+      true: "md:hover:w-fit md:hover:px-6 md:hover:py-4",
     },
   },
 });
